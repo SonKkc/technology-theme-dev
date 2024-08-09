377b92e312eef0121fbe6b6164056f2696508890
SonKkc <soncb.xyz@gmail.com>
2024-08-09 14:46:16 +0700

    Update navbar and list-card components, add loading component and fetchPosts function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById('loading-layout').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+
     const listItems = document.querySelectorAll("li");
 
     listItems.forEach(function (li) {
@@ -9,3 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+async function fetchPosts(endpoint, page = 1, limit = 16, ajax = 1) {
+    try {
+        const url = window.location.origin + endpoint + (endpoint.includes('?') ? '&' : '?') + 'page=' + page + '&limit=' + limit + '&ajax=' + ajax;
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.text();
+        const dataStr = data.replaceAll('\n', '').replaceAll('&quot;', '"');
+        const jsonData = JSON.parse(dataStr);
+        console.log(jsonData);
+        return jsonData;
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+        return { data: [] };
+    }
+}
+
+export { fetchPosts };
